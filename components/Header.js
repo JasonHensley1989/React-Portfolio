63b0d4bd29b7ad672c769b95e8c827cb62f49287
Jason <jasonhensley1989@gmail.com>
2022-04-17 19:55:29 -0400

    links created in header, working properly

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <Link className='header' to='/'>Home</Link>
+        <div>
+            <Link className='home' to='/'>Home</Link>
+            <Link className='about' to='/About'>About</Link>
+            <Link className='portfolio' to='/Portfolio'>Portfolio</Link>
+            <Link className='blog' to='/Blog'>Blog</Link>
+            <Link className='contact' to='/Contact'>Contact</Link>
+        </div>
     )
 }
 
