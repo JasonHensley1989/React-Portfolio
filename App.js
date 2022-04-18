c35e768f56a31f7371da309465e09bcdaac2e7e8
Jason <jasonhensley1989@gmail.com>
2022-04-17 20:03:50 -0400

    Canvas Component made

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import TopAnimation from './components/Top-Animation';
 
 function App() {
   return (
     <div className="App">
+      <TopAnimation />
       <Header />
       <Footer />
     </div>
---
+++ components/Top-Animation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const TopAnimation = () => {
+    return (
+        <div>Canvas Animation will go here!</div>
+    )
+}
+
+export default TopAnimation;
