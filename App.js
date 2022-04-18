5e9a2ac028d0cda8a80f79e46541218eeb8d283c
Jason <jasonhensley1989@gmail.com>
2022-04-17 20:17:19 -0400

    routes and links added, about created

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import TopAnimation from './components/Top-Animation';
+import About from './components/About';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <TopAnimation />
       <Header />
+      <Routes>
+        <Route path='/About' element={<About />}/>
+      </Routes>
       <Footer />
     </div>
   );
---
+++ components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const About = () => {
+    return (
+        <div>About Section will be here</div>
+    )
+}
+
+export default About;
